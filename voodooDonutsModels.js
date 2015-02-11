ed61dc85b0f618000426b644086f13b0d714f908
Lamson Le <ll217@u.washington.edu>
2015-02-11 14:54:36 -0800

    add checkbox and event listeners

--- voodooDonutsModels.js
+++ voodooDonutsModels.js
@@ -35,18 +35,46 @@
       };
       this.createCell(forecastTotal);
       return forecastTotal;
-    }
+    };
   };
 
-  var vDowntown = new Voodoo('Downtown', 'downtown', [700, 1800], [80, 220], 10, 4);
-  var vCapitolHill = new Voodoo('Capitol Hill', 'capitolhill', [700, 1800], [5, 45], 45, 2);
-  var vSouthLakeUnion = new Voodoo('South Lake Union', 'slu', [700, 1800], [180, 250], 5, 6);
-  var vWedgewood = new Voodoo('Wedgewood', 'wedgewood', [700, 1800], [20, 60], 20, 1.5);
-  var vBallard = new Voodoo('Ballard', 'ballard', [700, 1800], [25, 175], 33, 1);
+  var vDowntown = new Voodoo('Downtown', 'downtown', [700, 1800], [80, 220], 10, 4),
+      vCapitolHill = new Voodoo('Capitol Hill', 'capitolhill', [700, 1800], [5, 45], 45, 2),
+      vSouthLakeUnion = new Voodoo('South Lake Union', 'slu', [700, 1800], [180, 250], 5, 6),
+      vWedgewood = new Voodoo('Wedgewood', 'wedgewood', [700, 1800], [20, 60], 20, 1.5),
+      vBallard = new Voodoo('Ballard', 'ballard', [700, 1800], [25, 175], 33, 1);
 
-  vDowntown.forecast();
-  vCapitolHill.forecast();
-  vSouthLakeUnion.forecast();
-  vWedgewood.forecast();
-  vBallard.forecast();
+  var elDT = document.getElementById('cbDT'),
+      elCH = document.getElementById('cbCH'),
+      elSLU = document.getElementById('cbSLU'),
+      elWW = document.getElementById('cbWW'),
+      elBA = document.getElementById('cbBA');
 
+  function runForecast() {
+    if (this.checked) {
+      switch (this.id) {
+        case "cbDT":
+          vDowntown.forecast();
+          break;
+        case "cbCH":
+          vCapitolHill.forecast();
+          break;
+        case "cbSLU":
+          vSouthLakeUnion.forecast();
+          break;
+        case "cbWW":
+          vWedgewood.forecast();
+          break;
+        case "cbBA":
+          vBallard.forecast();
+          break;
+      }
+      this.disabled = true;
+    }
+  }
+
+  elDT.addEventListener('change', runForecast, false);
+  elCH.addEventListener('change', runForecast, false);
+  elSLU.addEventListener('change', runForecast, false);
+  elWW.addEventListener('change', runForecast, false);
+  elBA.addEventListener('change', runForecast, false);
